ac8d85d90be91559a69a31e25d766128a8fe8ca1
profxj <xavier@ucolick.org>
2017-05-28 07:14:42 -0700

    set width

--- frb/frb.py
+++ frb/frb.py
@@ -8,7 +8,7 @@ from . import utils
 class FRB(object):
     """
     """
-    def __init__(self, S, width, nu_c, DM, coord=None):
+    def __init__(self, S, nu_c, DM, coord=None):
         """
         Parameters
         ----------
@@ -23,11 +23,33 @@ class FRB(object):
           RA/DEC in one of many formats (see utils.radec_to_coord)
         """
         self.S = S
-        self.width = width
         self.nu_c = nu_c
         self.DM = DM
         # Coord
         if coord is not None:
             self.coord = utils.radec_to_coord(coord)
         #
+
+    def set_width(self, wtype, value, overwrite=False):
+        """ Set a Width value
+        Parameters
+        ----------
+        wtype : str
+          Indicates the type of width
+        value : Quantity
+        overwrite : bool, optional
+        """
+        # Restrict types
+        assert wtype in ['Wi', 'Wb', 'Wobs']  # Intrinsic, broadened, observed
+        # Check
+        if hasattr(self, wtype) and (not overwrite):
+            raise IOError("Width type {:s} is already set!".format(wtype))
+        # Vette
+        try:
+            value.to('s')
+        except:
+            raise IOError("Bad Quantity for value")
+        # Set
+        setattr(self, wtype, value)
+
 
