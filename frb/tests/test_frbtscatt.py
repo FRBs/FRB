aa72b6ef94c777eef9984d3aa122c6e6194f4cec
profxj <xavier@ucolick.org>
2017-10-07 08:10:05 -0700

    updated test

--- frb/tests/test_frbtscatt.py
+++ frb/tests/test_frbtscatt.py
@@ -20,15 +20,16 @@ def_l0 = 1*u.AU
 def_L0 = 0.001 * u.pc
 def_ne = 1e-2 / u.cm**3
 def_DL = 1 * u.kpc
+def_zL = 1.
 
 
 def test_init_turb():
     #
-    turb = Turbulence(def_ne, def_l0, def_L0)
+    turb = Turbulence(def_ne, def_l0, def_L0, def_zL)
     print(turb)
     assert turb.regime == 0
     # Try another beta
-    turb2 = Turbulence(def_ne, def_l0, def_L0, beta=3.)
+    turb2 = Turbulence(def_ne, def_l0, def_L0, def_zL, beta=3.)
     assert np.isclose(turb2.beta, 3.)
 
 
@@ -36,7 +37,7 @@ def test_temp_smearing():
     inu = 1 * u.GHz
     lobs = (const.c/inu).to('cm')
     # Galaxy at z=1
-    turb = Turbulence(def_ne, def_l0, def_L0, zL=1., DL=def_DL, lobs=lobs)
+    turb = Turbulence(def_ne, def_l0, def_L0, def_zL, DL=def_DL, lobs=lobs)
     # tau
     zsource = 2.
     tau = turb.temporal_smearing(lobs, zsource)
@@ -47,7 +48,7 @@ def test_temp_smearing():
 
 def test_ang_broadening():
     # Galaxy at z=1
-    turb = Turbulence(def_ne, def_l0, def_L0, zL=1.)
+    turb = Turbulence(def_ne, def_l0, def_L0, def_zL)
     inu = 1 * u.GHz
     lobs = (const.c/inu).to('cm')
     # SM, rdiff
