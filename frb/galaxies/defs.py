b3f0621f561038d34cc90b738252c84562e3dfa5
profxj <xavier@ucolick.org>
2019-01-28 14:04:14 -0800

    cigale parsing

--- frb/galaxies/defs.py
+++ frb/galaxies/defs.py
@@ -1,3 +1,31 @@
 """ Define allowed quantities for FRB galaxies"""
 
-valid_filters = ('DES_r')
+##############################################################
+# Photometry
+
+# Filters
+
+valid_filters = []  # Uncertainty is valid with '_err' add-on, eg. W1_err
+
+# DES
+DES_bands = ['g', 'r', 'i', 'z', 'Y']
+for band in DES_bands:
+    valid_filters.append('DES_{:s}'.format(band))
+
+# WISE
+WISE_bands = ['W1', 'W2', 'W3', 'W4']
+for band in WISE_bands:
+    valid_filters.append('{:s}'.format(band))
+
+##############################################################
+# Derived quantities
+
+valid_derived_photom = [
+    'Mstar',           # Stellar mass; linear in Msun CIGALE
+    'f_AGN',           # Fraction of AGN contribution to light; CIGALE
+    'u-r',             # Rest-frame; CIGALE
+    'M_r',             # Absolute magnitude, r-band rest-frame; CIGALE+
+    'SFR_photom',      # SFR in Msun/yr from photometry; CIGALE
+    'EBV_photom',      # E(B-V) from photometry; CIGALE
+    'Z_photom',        # Metallicity from photometry; CIGALE
+    ]
--- frb/galaxies/frbgalaxy.py
+++ frb/galaxies/frbgalaxy.py
@@ -20,6 +20,10 @@ from astropy.cosmology import z_at_value
 from astropy import constants
 from astropy.table import Table
 
+from linetools import utils as ltu
+
+from frb.galaxies import defs
+
 
 class FRBGalaxy(object):
     """
@@ -42,12 +46,78 @@ class FRBGalaxy(object):
     def parse_photom(self, phot_tbl, max_off=1*units.arcsec, overwrite=True):
         phot_coord = SkyCoord(ra=phot_tbl['ra'], dec=phot_tbl['dec'], unit='deg')
         sep = self.coord.separation(phot_coord)
-        imin = np.argmin(sep)
+        row = np.argmin(sep)
         # Satisfy minimum offset?
-        if sep[imin] > max_off:
+        if sep[row] > max_off:
             print("No photometric sources within {} of the galaxy".format(max_off))
             return
         # Fill
+        for filter in defs.valid_filters:
+            # Value
+            if filter in phot_tbl.keys():
+                if (filter in self.photom.keys()) and (not overwrite):
+                    pass
+                else:
+                    self.photom[filter] = phot_tbl[filter][row]
+                    # Try error
+                    if filter+'_err' in phot_tbl.keys():
+                        self.photom[filter+'_err'] = phot_tbl[filter+'_err'][row]
+
+    def parse_cigale(self, cigale_file, overwrite=True):
+        # Read
+        cigale_tbl = Table.read(cigale_file)
+
+        # Derived quantities
+        cigale_translate = [
+            ('Mstar', 'bayes.stellar.m_star'),
+            ('f_AGN', 'bayes.agn.fracAGN_dale2014'),
+            ('u-r', 'bayes.param.restframe_u_prime-r_prime')
+        ]
+        # Do it
+        cigale = {}
+        for item in cigale_translate:
+            assert item[0] in defs.valid_derived_photom
+            if item[1] in cigale_tbl.keys():
+                cigale[item[0]] = cigale_tbl[item[1]][0]          # Solar masses, linear
+                cigale[item[0]+'_err'] = cigale_tbl[item[1]+'_err'][0]          # Solar masses, linear
+        # Fill Derived
+        for key, item in cigale.items():
+            if (key not in self.derived.keys()) or (overwrite):
+                self.derived[key] = item
+
+    def vette_dict(self, dict, valid_defs):
+        pass
+
+    def write_to_json(self, outfile=None):
+        if outfile is None:
+            jname = ltu.name_from_coord(self.coord)
+            outfile = jname+'_FRB{}.json'.format(self.frb)
+        # Build the dict
+        frbgal_dict = {}
+
+        # Basics
+        frbgal_dict['ra'] = self.coord.ra.value
+        frbgal_dict['dec'] = self.coord.dec.value
+        frbgal_dict['FRB'] = self.frb
+
+        # Photometry
+        if len(self.photom) > 0:
+            frbgal_dict['photom'] = self.photom
+
+        # JSONify
+        jdict = ltu.jsonify(frbgal_dict)
+
+        # Write
+        ltu.savejson(outfile, jdict, easy_to_read=True, overwrite=True)
+        print("Wrote data to {}".format(outfile))
+
+    def __repr__(self):
+        txt = '<{:s}: {:s} {:s}, FRB={:s}'.format(
+            self.__class__.__name__, self.coord.icrs.ra.to_string(unit=units.hour,sep=':', pad=True),
+            self.coord.icrs.dec.to_string(sep=':',pad=True,alwayssign=True), self.frb)
+        # Finish
+        txt = txt + '>'
+        return (txt)
 
 
 class FRBHost(FRBGalaxy):
