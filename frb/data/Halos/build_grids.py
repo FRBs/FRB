1a1c6b39407c4bfaf6cc7bc778470c18f0568bb6
profxj <xavier@ucolick.org>
2019-01-28 10:38:35 -0800

    script

--- frb/data/Halos/build_grids.py
+++ frb/data/Halos/build_grids.py
@@ -5,8 +5,11 @@ if __name__ == '__main__':
     #build_grid(outfile='z1_mNFW_10000', ntrial=10000)
     #build_grid(outfile='z1_mNFW_10000_21dec2018', ntrial=10000)
     #build_grid(outfile='test', ntrial=500, r_max=1.)
-    build_grid(outfile='test', ntrial=100, r_max=1.)
+    #build_grid(outfile='test', ntrial=100, r_max=1.)
     #build_grid(outfile='test', ntrial=10)
 
     # Fiducial model
     #build_grid(outfile='z1_mNFW_10000_rmax1', ntrial=10000, r_max=1.)
+
+    # rmax=2
+    build_grid(outfile='z1_mNFW_10000_rmax2', ntrial=10000, r_max=2.)
