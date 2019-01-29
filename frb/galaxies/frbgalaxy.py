03058da5e9f35fe0b86ea228ef56eb2e8040502a
Sunil Simha <sunil.simha@gmail.com>
2019-01-28 20:33:32 -0800

    Made gen_cigale_data_in a method of FRBGalaxy

--- frb/galaxies/frbgalaxy.py
+++ frb/galaxies/frbgalaxy.py
@@ -108,6 +108,54 @@ class FRBGalaxy(object):
                     # Try error
                     if filter+'_err' in phot_tbl.keys():
                         self.photom[filter+'_err'] = phot_tbl[filter+'_err'][row]
+    
+    def gen_cigale_data_in(self, ID=None,filename='data.fits',overwrite=False):
+        """
+        Generates the input data file for CIGALE
+        given the photometric points and redshift
+        of a galaxy
+        Parameters
+        ----------
+        self: FRBGalaxy or FRBHost object
+            A Galaxy object having redshift estimates
+            and photometric data available.
+        ID: str, optional
+            An ID for the galaxy. If none, "GalaxyA" is assigned.
+        filename: str, optional
+            Name of fits file (with path if needed) to store data in.
+            Default value is 'data.fits'
+        overwrite = bool, optional
+            If true, previously written fits files will be
+            overwritten
+        """
+        assert (self.photom != {}),"No photometry found. CIGALE cannot be run."
+        assert (self.redshift != {}),"No redshift found. CIGALE cannot be run"
+
+        photom = self.photom
+        if ID is None:
+            ID = "GalaxyA"
+        photom['id'] = ID
+        photom['redshift'] = self.z
+        
+        #Convert DES fluxes to mJy
+        for band in defs.DES_bands:
+            colname = "DES_"+band
+            photom[colname] = 3630780.5*10**(photom[colname]/-2.5)
+            photom[colname+"_err"] = photom[colname+"_err"]/1.087*photom[colname]
+        
+        #Convert WISE fluxes to mJy
+        wise_fnu0 = [309.54,171.787,31.674,8.363] #http://wise2.ipac.caltech.edu/docs/release/allsky/expsup/sec4_4h.html#conv2flux
+        for band,zpt in zip(defs.WISE_bands,wise_fnu0):
+            photom[band] = zpt*10**(-photom[band]/2.5)
+            errname = band+"_err"
+            if photom[errname]!=-999.0:
+                photom[errname] =-99.0
+            else:
+                photom[errname] = photom[errname]/1.087*photom[band]
+        
+        #Write to file
+        photom = Table([photom])
+        photom.write(filename,format="fits",overwrite=overwrite)
 
     def parse_cigale(self, cigale_file, overwrite=True):
         # Read
