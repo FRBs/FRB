3dcd44d0dea34c1254b2948c76473c64be352c4f
profxj <xavier@ucolick.org>
2019-05-14 09:45:52 -0700

    survey test..

--- frb/surveys/dlsurvey.py
+++ frb/surveys/dlsurvey.py
@@ -7,7 +7,7 @@ import pdb
 import numpy as np
 import warnings
 from astropy.table import Table
-from astropy import units, io, utils
+from astropy import units
 try:
     from dl import queryClient as qc, authClient as ac, helpers
 except ImportError:
