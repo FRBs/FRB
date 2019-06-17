032baf0596dfa105a4b6453061c8ed5d99b5735c
profxj <xavier@ucolick.org>
2019-06-17 08:54:18 -0700

    SDSS redshifts

--- frb/specdb/build.py
+++ frb/specdb/build.py
@@ -11,11 +11,19 @@ import glob
 
 from pkg_resources import resource_filename
 
+from astropy.coordinates import SkyCoord
+from astropy.coordinates import match_coordinates_sky
+from astropy import units
+from astropy.table import Table
+
 from specdb import defs
 from specdb.build import privatedb as pbuild
 from specdb.build import utils as spbu
 
+from frb.surveys import sdss
+
 all_instruments = ['SDSS']
+spectra_path = resource_filename('frb', '../Spectra')
 
 def grab_files(all_files, instrument):
     # Setup
@@ -27,11 +35,61 @@ def grab_files(all_files, instrument):
             file_subset.append(all_files[kk])
     # Return
     return file_subset
-    
+
+
+def sdss_redshifts():
+    """
+    Enter the SDSS directory and build a redshift table
+    based on the spectra present
+
+    Returns:
+
+    """
+    #
+    all_folders = glob.glob(spectra_path+'/SDSS/*')
+    Jnames = []
+    for folder in all_folders:
+        # Grab the list of spectra files
+        spec_files = glob.glob(os.path.join(folder, 'J*.fits'))
+        # Generate the name list
+        Jnames += [os.path.basename(ifile).split('_')[0] for ifile in spec_files]
+    # Coords
+    coords = SkyCoord(Jnames, unit=(units.hourangle, units.deg))  # from DES
+    # Setup
+    done = np.zeros_like(coords.ra.value, dtype=bool)
+    zs = np.zeros_like(coords.ra.value)
+
+    # Loop me
+    while np.any(~done):
+        # Grab the first not done
+        i0 = np.where(~done)[0][0]
+        coord = coords[i0]
+        # Grab the SDSS data
+        sdssSurvey = sdss.SDSS_Survey(coord, 10*units.arcmin)
+        #
+        sub_coords = coords[~done]
+        sep = coord.separation(sub_coords)
+        doidx = np.where(sep < 10*units.arcmin)[0]
+        dothem = coords[doidx]
+        # Now match
+        catalog = sdssSurvey.get_catalog()
+        sdss_coords = SkyCoord(ra=catalog['ra'], dec=catalog['dec'], unit='deg')
+        idx, d2d, d3d = match_coordinates_sky(dothem, sdss_coords, nthneighbor=1)
+        # Fill
+        zs[doidx] = catalog['z_spec'][idx]
+        done[np.where(dothem)] = True
+
+    # Write the catalog
+    tbl = Table()
+    tbl['ra'] = coords.ra.value
+    tbl['dec'] = coords.dec.value
+    tbl['z'] = zs
+    tbl['z_source'] = 'SDSS'
+    tbl.write('z_SDSS.ascii', overwrite=True, format='ascii.fixed_width')
+
     
 def generate_by_refs(input_refs, outfile):
     # Not elegant but it works
-    spectra_path = resource_filename('frb', '../Spectra')
     all_folders = glob.glob(spectra_path+'/*/*')
     all_refs = [os.path.basename(ifolder) for ifolder in all_folders]
     
@@ -83,4 +141,5 @@ def generate_by_refs(input_refs, outfile):
 if __name__ == '__main__':
 
     # Test
-    generate_by_refs(['DR7'], 'tst_specdb.hdf5')
+    #generate_by_refs(['DR7'], 'tst_specdb.hdf5')
+    sdss_redshifts()
