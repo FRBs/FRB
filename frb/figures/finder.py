cec2e4ace2965aa438f34c2a88c0860637749420
profxj <xavier@ucolick.org>
2019-09-20 20:50:57 -0700

    SDSS finder

--- frb/figures/finder.py
+++ frb/figures/finder.py
@@ -5,6 +5,10 @@ import numpy as np
 from IPython import embed
 
 from matplotlib import pyplot as plt
+from matplotlib import font_manager
+import matplotlib.cm as cm
+
+from PIL import Image
 
 from astropy import units
 from astropy.visualization.wcsaxes import SphericalCircle
@@ -12,10 +16,14 @@ from astropy.stats import sigma_clipped_stats
 from astropy.visualization import LogStretch, mpl_normalize as mplnorm
 from astropy.nddata import Cutout2D
 from astropy.wcs import WCS
+from astropy.coordinates import ICRS
+from astropy.time import Time
 
 from linetools import utils as ltu
 
 from frb.figures import utils
+from frb.surveys import survey_utils
+from frb.surveys import images
 
 
 def from_hdu(hdu, title, **kwargs):
@@ -183,3 +191,233 @@ def generate(image, wcs, title, log_stretch=False,
 
     # Return
     return fig, ax
+
+
+#### ###############################
+#  Main driver
+#  finder.main(['TST', '10:31:38.87', '+25:59:02.3'])
+#  imsize is in arcmin
+def sdss_dss(coord, title, survey='2r', show_circ=True,
+             EPOCH=None, DSS=None, BW=False, imsize=5.*units.arcmin,
+             outfile=None,
+         show_slit=None, show_another=None, cradius=None,
+         in_img=None):
+    """
+    Generate a SDSS or DSS finder
+
+    Pulled from xastropy
+
+    Args:
+        coord:
+        title:
+        survey:
+        radec:
+        deci:
+        fpath:
+        show_circ (bool, optional):
+            Show a yellow circle on the target
+        EPOCH:
+        DSS:
+        BW (bool, optional):
+           B&W image?
+        imsize:
+        show_slit:
+        OUT_TYPE:
+        show_another:
+        cradius (Quantity, optional):
+            Circle radius, only shown if show_circ is True.
+            Default is imsize/50.
+        in_img:
+
+    show_another : tuple, optional
+       RA,DEC for another target to circle (e.g. offset star)
+    show_slit: Input
+        Whether to overplot a slit
+        None - No show
+        List of values - [width, length, PA], e.g. [1*u.arcsec, 10*u.arcsec, 20*u.deg]
+    imsize: Quantity, optional
+       Image size
+    OUT_TYPE: str, optional
+       File type -- 'PDF', 'PNG'
+    """
+    # Init
+    vimsize = imsize.to('arcmin').value
+    if cradius is None:
+        cradius = vimsize / 50.
+    else:
+        cradius = cradius.to('arcmin').value
+
+    # Precess (as necessary)
+    if EPOCH is not None:
+        # Precess to 2000.
+        tEPOCH = Time(EPOCH, format='jyear', scale='utc')
+        # Load into astropy
+        icrs = ICRS(ra=coord.ra.value, dec=coord.dec.value, equinox=tEPOCH, unit='deg')
+        # Precess
+        newEPOCH = Time(2000., format='jyear', scale='utc')
+        coord = icrs.precess_to(newEPOCH)
+
+    # Grab the Image
+    if in_img is None:
+        img, oBW = getjpg(coord, imsize)#, BW=BW, DSS=DSS)
+    else:
+        img = in_img
+        oBW = True
+
+    # Generate the plot
+    plt.clf()
+    fig = plt.figure(dpi=700)
+    fig.set_size_inches(8.0, 10.5)
+
+    # Font
+    plt.rcParams['font.family'] = 'times new roman'
+    ticks_font = font_manager.FontProperties(family='times new roman', style='normal',
+                                             size=16, weight='normal', stretch='normal')
+    ax = plt.gca()
+    for label in ax.get_yticklabels():
+        label.set_fontproperties(ticks_font)
+    for label in ax.get_xticklabels():
+        label.set_fontproperties(ticks_font)
+
+    # Image
+    if oBW == 1:
+        cmm = cm.Greys
+    else:
+        cmm = None
+    plt.imshow(img, cmap=cmm, aspect='equal', extent=(-vimsize / 2., vimsize / 2,
+                                                      -vimsize / 2., vimsize / 2))
+
+    # Axes
+    plt.xlim(-vimsize / 2., vimsize / 2.)
+    plt.ylim(-vimsize / 2., vimsize / 2.)
+
+    # Label
+    plt.xlabel('Relative ArcMin', fontsize=20)
+    xpos = 0.12 * vimsize
+    ypos = 0.02 * vimsize
+    plt.text(-vimsize / 2. - xpos, 0., 'EAST', rotation=90., fontsize=20)
+    plt.text(0., vimsize / 2. + ypos, 'NORTH', fontsize=20, horizontalalignment='center')
+
+    # import pdb; pdb.set_trace()
+
+    # Circle
+    if show_circ:
+        circle = plt.Circle((0, 0), cradius, color='y', fill=False)
+        plt.gca().add_artist(circle)
+
+    # Second Circle
+    if show_another is not None:
+        # Coordinates
+        canother = coord.to_coord(show_another)
+        embed(header='338')
+        # Offsets
+        xanother = -1 * off[0].to('arcmin').value
+        yanother = off[1].to('arcmin').value
+        square = matplotlib.patches.Rectangle((xanother - cradius,
+                                               yanother - cradius),
+                                              cradius * 2, cradius * 2, color='cyan', fill=False)
+        plt.gca().add_artist(square)
+        plt.text(0.5, 1.24, str(nm), fontsize=32,
+                 horizontalalignment='center', transform=ax.transAxes)
+        plt.text(0.5, 1.18, 'RA (J2000) = ' + str(obj['RAS']) +
+                 '  DEC (J2000) = ' + str(obj['DECS']), fontsize=22,
+                 horizontalalignment='center', transform=ax.transAxes)
+        plt.text(0.5, 1.12, 'RA(other) = {:s}  DEC(other) = {:s}'.format(
+            canother.ra.to_string(unit=astrou.hour, pad=True, sep=':', precision=2),
+            canother.dec.to_string(pad=True, alwayssign=True, sep=':', precision=1)),
+                 fontsize=22, horizontalalignment='center', transform=ax.transAxes,
+                 color='blue')
+        plt.text(0.5, 1.06, 'RA(to targ) = {:.2f}  DEC(to targ) = {:.2f} PA={:g}'.format(
+            -1 * off[0].to('arcsec'), -1 * off[1].to('arcsec'), PA),
+                 fontsize=18, horizontalalignment='center', transform=ax.transAxes)
+    else:
+        # Title
+        plt.text(0.5, 1.24, str(title), fontsize=32,
+                 horizontalalignment='center', transform=ax.transAxes)
+        ras = coord.ra.to_string(unit=units.hour,sep=':',pad=True,precision=2)
+        decs = coord.dec.to_string(sep=':',pad=True, alwayssign=True,precision=1)
+        plt.text(0.5, 1.16, 'RA (J2000) = ' + ras, fontsize=28,
+                 horizontalalignment='center', transform=ax.transAxes)
+        plt.text(0.5, 1.10, 'DEC (J2000) = ' + decs, fontsize=28,
+                 horizontalalignment='center', transform=ax.transAxes)
+
+    # Show slit??
+    if show_slit is not None:
+        # List of values - [width, length, PA],
+        # e.g. [1*u.arcsec, 10*u.arcsec, 20*u.deg]
+        w, l, pa = show_slit
+        w_arcmin = w.to('arcmin').value
+        l_arcmin = l.to('arcmin').value
+        pa_deg = pa.to('deg').value
+        pa_rad = pa_deg * np.pi / 180.
+        # get the new position of the lower-left corner of rectangle given the PA
+        y_new = 0. - 0.5 * (l_arcmin * np.sin(np.pi / 2. - pa_rad) + w_arcmin * np.sin(pa_rad))
+        x_new = 0. + 0.5 * (l_arcmin * np.cos(np.pi / 2. - pa_rad) - w_arcmin * np.cos(pa_rad))
+        xy = (x_new, y_new)  # xy of lower-left corner (after rotation)
+        box = plt.Rectangle(xy, w_arcmin, l_arcmin, color='k', angle=pa_deg, fill=False, lw=0.5)
+        plt.gca().add_artist(box)
+        plt.text(0.5, 0.05, 'Slit PA={}deg'.format(pa_deg),
+                 fontsize=15, ha='center', va='top', transform=ax.transAxes)
+
+    if outfile is not None:
+        print("Writing: {}".format(outfile))
+        plt.savefig(outfile)
+        plt.close()
+    else:
+        plt.show()
+
+
+# ##########################################
+def getjpg(coord, imsize, BW=False, DSS=None):
+    """
+    Grab an SDSS or DSS image
+
+    Args:
+        ira:
+        idec:
+        imsize:
+        BW:
+        DSS:
+
+    Returns:
+
+    """
+    sdss = survey_utils.load_survey_by_name('SDSS', coord, 0.02*units.deg)
+    # Dummy call to see if SDSS covers it
+    cat = sdss.get_catalog()
+    if cat is None:
+        print("No SDSS Image;  Querying DSS")
+        BW = True
+        url = dsshttp(coord, imsize)  # DSS
+        img = images.grab_from_url(url)
+    else:
+        img, _ = sdss.get_cutout(imsize)
+    return img, BW
+
+
+def dsshttp(coord, imsize):
+    """
+    Generate URL for DSS
+
+    Args:
+        coord:
+        imsize:
+
+    Returns:
+
+    """
+    # https://archive.stsci.edu/cgi-bin/dss_search?v=poss2ukstu_red&r=00:42:44.35&d=+41:16:08.6&e=J2000&h=15.0&w=15.0&f=gif&c=none&fov=NONE&v3=
+
+    Equinox = 'J2000'
+    dss = 'poss2ukstu_red'
+    url = "http://archive.stsci.edu/cgi-bin/dss_search?"
+    url += "v=" + dss + '&r=' + str(coord.ra.value) + '&d=' + str(coord.dec.value)
+    url += "&e=" + Equinox
+    url += '&h=' + str(imsize) + "&w=" + str(imsize)
+    url += "&f=gif"
+    url += "&c=none"
+    url += "&fov=NONE"
+    url += "&v3="
+
+    return url
+
