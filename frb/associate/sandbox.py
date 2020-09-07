6743c3331febe4f5d2b67513f9406c90f10f3134
profxj <xavier@ucolick.org>
2020-09-07 07:32:33 -0700

    WIP

---
+++ frb/associate/sandbox.py
@@ -0,0 +1,105 @@
+""" Module for methods related to building FRB Assocation sandboxes"""
+
+import numpy as np
+
+from astropy.coordinates import SkyCoord
+from astropy.table import Table
+from astropy import units
+
+from frb import frb
+
+from IPython import embed
+
+def theta_uniform(theta_max, rstate, ntheta):
+    dtheta = rstate.uniform(size=ntheta, low=0., high=theta_max)
+    rdir = rstate.uniform(size=ntheta, low=0., high=2*np.pi)
+    #
+    theta_ra = dtheta * np.cos(rdir)
+    theta_dec = dtheta * np.sin(rdir)
+    # Return
+    return theta_ra, theta_dec
+
+def offset_frb(sigR, rstate, nFRB, max_sig=5.):
+    roff = rstate.normal(scale=sigR, size=nFRB)
+    rdir = rstate.uniform(size=nFRB, low=0., high=2*np.pi)
+    # Turn into ra, dec
+    roff = np.minimum(roff, max_sig*sigR)  # Note this includes negative values
+    frb_ra = roff * np.cos(rdir)
+    frb_dec = roff * np.sin(rdir)
+    # Return
+    return frb_ra, frb_dec
+
+
+def build(source_tbl, field, phot_col, zmnx, Lmnx, theta, sigR, nSand=100,
+          seed=12345, edge_buff=20*units.arcsec):
+
+    # Random numbers
+    rstate = np.random.RandomState(seed)
+
+    # Choose random redshifts
+    zval = rstate.uniform(size=nSand, low=zmnx[0], high=zmnx[1])
+
+    # Convert z to r_mag from Lmnx.  Cheating for now
+    rmags = rstate.uniform(size=nSand, low=19., high=22.)
+
+    # Sandbox edge
+    field_coord = SkyCoord(ra=field[0], dec=field[1], unit='deg')
+    obj_coord = SkyCoord(ra=source_tbl['ra'], dec=source_tbl['dec'], unit='deg')
+    obj_sep = field_coord.separation(obj_coord)
+    ok_sep = obj_sep < (field[2]*units.deg - edge_buff)
+
+    # Collect the objects
+    chosen = np.zeros(len(source_tbl), dtype=bool)
+    delta_r = 0.2  # mag
+
+    objs = []
+    for kk in range(nSand):
+        rmag = rmags[kk]
+        # Pick one
+        options = (np.abs(source_tbl[phot_col] - rmag) < delta_r) & np.logical_not(chosen) & ok_sep
+        noptions = np.sum(options)
+        if noptions < 10:
+            embed(header='29 of sandbox')
+
+        # Random select
+        rand = int(rstate.randint(low=0, high=noptions-1, size=1))
+        iobj = np.where(options)[0][rand]
+        objs.append(iobj)
+        chosen[iobj] = True
+    objs = np.array(objs)
+
+    # Offset
+    if theta['method'] == 'uniform':
+        thetas_ra, thetas_dec = theta_uniform(theta['max'], rstate, nSand)
+    else:
+        embed(header='45')
+
+    # FRB offset
+    frb_off_ra, frb_off_dec = offset_frb(sigR, rstate, nSand)
+
+    # Apply
+    frb_coords = obj_coord[objs]
+    frb_ra = frb_coords.ra.value + thetas_ra/3600. + frb_off_ra/3600.
+    frb_dec = frb_coords.dec.value + thetas_dec/3600. + frb_off_dec/3600.
+
+    # Final table
+    frb_tbl = Table()
+    frb_tbl[phot_col] = rmags
+    frb_tbl['ra'] = frb_ra
+    frb_tbl['dec'] = frb_dec
+    frb_tbl['iobj'] = objs
+    frb_tbl['obj_ra'] = obj_coord[objs].ra.value
+    frb_tbl['obj_dec'] = obj_coord[objs].dec.value
+
+    # Return
+    return frb_tbl
+
+# Command line execution
+if __name__ == '__main__':
+    # Test
+    source_tbl = Table.read('tst_DES_180924.fits')
+    field = source_tbl.meta['RA'], source_tbl.meta['DEC'], source_tbl.meta['RSEARCH']
+    theta = dict(method='uniform', max=2.)
+    frb_tbl = build(source_tbl, field, 'DES_r', (0.2, 0.4), (0.1, 1.), theta, 0.25)
+    frb_tbl.write('tst_FRB_180924.fits', overwrite=True)
+
