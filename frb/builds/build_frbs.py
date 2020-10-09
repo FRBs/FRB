872e7ebab86f74241b5e2544e09f65a23b5e6cc7
keheintz <keh14@hi.is>
2020-10-09 11:01:27 +0000

    Updated build for 190523

--- frb/builds/build_frbs.py
+++ frb/builds/build_frbs.py
@@ -173,7 +173,7 @@ def frb_190523():
                         760.8 * units.pc / units.cm**3,
                         z_frb=0.660)
     # Error ellipse [REQUIRED]
-    frb190523.set_ee(4, 1.5, theta=0., cl=68.) # Halfing the 3x8'' at 95% reported by Ravi et al.
+    frb190523.set_ee(4, 1.5, cl=68., theta=340.) # Halfing the 3x8'' at 95% reported by Ravi et al.
     # Error in DM
     frb190523.DM_err = 0.6 * units.pc / units.cm**3
 
