10d648e68f39400b473419e9f0c44c5ebab2ae24
EmPlatts <em.d.platts@gmail.com>
2020-11-03 18:16:25 +0200

    fixed psr def

--- dmgap/transient_kdes.py
+++ dmgap/transient_kdes.py
@@ -2,21 +2,13 @@
 Standard KDE is used for pulsars and varying assymetric KDE for FRBs.
 To use this code, please download the asymmetric_kde package from
 https://github.com/tillahoffmann/asymmetric_kde [Hoffman and Jones, 2015] """
-
 from asymmetric_kde import ProperGammaEstimator
-from frb import igm
+from pdf_defs import make_kde_funtion
 import numpy as np
-import scipy as sp
 import pandas as pd
-from pandas import DataFrame as df
-import matplotlib
-import matplotlib.pyplot as plt
-from scipy.stats import lognorm
 from scipy.signal import argrelextrema
-from pdf_fns import make_kde_funtion, make_pdf, rv_amount
-
-from astropy.cosmology import WMAP9 as cosmo
-from astropy import units
+from astropy.stats import bootstrap
+from astropy.utils import NumpyRNGContext
 import random
 
 def kde_for_frb(grid, dm_frb, num_samples, num_resamples=100, save_to_path=None):
@@ -53,7 +45,7 @@ def kde_for_frb(grid, dm_frb, num_samples, num_resamples=100, save_to_path=None)
         ige_func = ige_sample(dm_grid) #KDEs
         frb_p = np.diff(ige_func)
         frb_lim_sample = dm_grid_p[argrelextrema(frb_p, np.greater)[0][0]] #max gradient at front of PDF
-        frb_lim = np.append(frb_lim,frb_lim_sample)
+        frb_lim.append(frb_lim_sample)
         dm_kde.append(ige_func)
 
     if save_to_path==None:
@@ -64,7 +56,7 @@ def kde_for_frb(grid, dm_frb, num_samples, num_resamples=100, save_to_path=None)
 
     return dm_kde, frb_lim
 
-def kde_for_psr(grid, dm_psr, num_samples, num_resamples=100, save_to_path=None):
+def kde_for_psr(grid, dm_psr, num_samples, num_resamples=100, min_bandwidth=3, max_bandwidth=15, bandwidth_stepsize=0.1, cv=100, kernel='gaussian', save_to_path=None):
     """
     Find pulsar KDE and maxima
     
@@ -74,9 +66,19 @@ def kde_for_psr(grid, dm_psr, num_samples, num_resamples=100, save_to_path=None)
         dm_psr (array):
             PDF of DM_psr.
         num_samples (int):
-            Number of draws from dm_frb_obs.
+            Number of draws from dm_psr
         num_resamples (int, optional):
             Number of times to resample the draws. Default=100.
+        min_bandwidth (int, optional):
+            Minimum bandwidth for cross-validation. Default=8.
+        max_bandwidth (int, optional):
+            Maximum bandwidth for cross-validation. Default=15.
+        bandwidth_stepsize (int, optional):
+            Bandwidth stepsize. Default=1.
+        cv (int, optional):
+            Number of folds for cross-validation. Default=5.
+        kernel (str, optional):
+            Kernel to use. Default='gaussian'
         save_to_path (str, optional):
             To save data outputs (as .npy), specify path. Default=None.
 
@@ -88,24 +90,23 @@ def kde_for_psr(grid, dm_psr, num_samples, num_resamples=100, save_to_path=None)
             The max gradients of the pulsar DM KDEs, corresponding to an lower limit on the MW halo DM
 
     """
-    dm_frb = dm_obs
     dm_grid = grid
     dm_grid_p = dm_grid[1:] #grid for the derivative 
-    frb_lim = []
+    psr_lim = []
     dm_kde = []
-    for _ in range(num_resamples):
-        dm_sample = np.sort(np.asarray(random.sample(list(dm_frb),num_samples)))
-        ige_sample = ProperGammaEstimator(dm_sample, 'plugin')
-        ige_func = ige_sample(dm_grid) #KDEs
-        frb_p = np.diff(ige_func)
-        frb_lim_sample = dm_grid_p[argrelextrema(frb_p, np.greater)[0][0]] #max gradient at front of PDF
-        frb_lim = np.append(frb_lim,frb_lim_sample)
-        dm_kde.append(ige_func)
+    with NumpyRNGContext(1):
+        boot_obs_psr = bootstrap(dm_psr, num_resamples)
+    for i in range(num_resamples):
+        kde_func = make_kde_funtion(grid=dm_grid, draws=boot_obs_psr[i], min_bandwidth=min_bandwidth, max_bandwidth=max_bandwidth, bandwidth_stepsize=bandwidth_stepsize, cv=cv, kernel=kernel)
+        psr_p = np.diff(kde_func)
+        psr_lim_sample = dm_grid_p[psr_p.argmin()]
+        psr_lim.append(psr_lim_sample)
+        dm_kde.append(kde_func)
 
     if save_to_path==None:
         pass
     else:
-        np.save(save_to_path, frb_lim)
+        np.save(save_to_path, psr_lim)
         np.save(save_to_path, dm_kde)
 
-    return dm_kde, frb_lim
+    return dm_kde, psr_lim
