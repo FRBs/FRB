37273f1e9c3207678b4f89db24790064db23d52b
EmPlatts <em.d.platts@gmail.com>
2020-11-11 14:15:49 +0200

    Update transient_kdes.py

--- frb/dm_kde/transient_kdes.py
+++ frb/dm_kde/transient_kdes.py
@@ -2,14 +2,18 @@
 Standard KDE is used for pulsars and varying assymetric KDE for FRBs.
 To use this code, please download the asymmetric_kde package from
 https://github.com/tillahoffmann/asymmetric_kde [Hoffman and Jones, 2015] """
+
 from asymmetric_kde import ProperGammaEstimator
-from pdf_defs import make_kde_funtion
 import numpy as np
 import pandas as pd
 from scipy.signal import argrelextrema
+from pdf_fns import make_kde_funtion
 from astropy.stats import bootstrap
 from astropy.utils import NumpyRNGContext
 import random
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
 def kde_for_frb(grid, dm_frb, num_samples, num_resamples=100, save_to_path=None):
     """
@@ -37,8 +41,14 @@ def kde_for_frb(grid, dm_frb, num_samples, num_resamples=100, save_to_path=None)
     """
     dm_grid = grid
     dm_grid_p = dm_grid[1:] #grid for the derivative 
+    logging.info('Finding optimal KDE...')
+    ige = ProperGammaEstimator(dm_frb, 'plugin')
+    dm_kde_opt = ige(dm_grid) #KDEs
+    frb_p_opt = np.diff(dm_kde_opt)
+    frb_lim_opt = dm_grid_p[argrelextrema(frb_p_opt, np.greater)[0][0]] 
     frb_lim = []
     dm_kde = []
+    logging.info('Bootstrapping KDE...')
     for _ in range(num_resamples):
         dm_sample = np.sort(np.asarray(random.sample(list(dm_frb),num_samples)))
         ige_sample = ProperGammaEstimator(dm_sample, 'plugin')
@@ -51,12 +61,15 @@ def kde_for_frb(grid, dm_frb, num_samples, num_resamples=100, save_to_path=None)
     if save_to_path==None:
         pass
     else:
-        np.save(save_to_path, frb_lim)
-        np.save(save_to_path, dm_kde)
+        np.save(save_to_path+'/kde_frb_lim.npy', frb_lim)
+        np.save(save_to_path+'/kde_frb_lim_opt.npy', frb_lim_opt)
+        np.save(save_to_path+'/kde_frb.npy', dm_kde)
+        np.save(save_to_path+'/kde_frb_opt.npy', dm_kde_opt)
+        logging.info('Output saved to '+str(save_to_path))
 
     return dm_kde, frb_lim
 
-def kde_for_psr(grid, dm_psr, num_samples, num_resamples=100, min_bandwidth=3, max_bandwidth=15, bandwidth_stepsize=0.1, cv=100, kernel='gaussian', save_to_path=None):
+def kde_for_psr(grid, dm_psr, num_samples, num_resamples=100, min_bandwidth=8, max_bandwidth=15, bandwidth_stepsize=0.1, cv=100, kernel='gaussian', save_to_path=None):
     """
     Find pulsar KDE and maxima
     
@@ -91,11 +104,16 @@ def kde_for_psr(grid, dm_psr, num_samples, num_resamples=100, min_bandwidth=3, m
 
     """
     dm_grid = grid
-    dm_grid_p = dm_grid[1:] #grid for the derivative 
+    dm_grid_p = dm_grid[1:] #grid for the derivative
+    logging.info('Finding optimal KDE...')
+    dm_kde_opt = make_kde_funtion(grid=dm_grid, draws=dm_psr, min_bandwidth=min_bandwidth, max_bandwidth=max_bandwidth, bandwidth_stepsize=bandwidth_stepsize, cv=cv, kernel=kernel)
+    psr_p_opt = np.diff(dm_kde_opt)
+    psr_lim_opt = dm_grid_p[psr_p_opt.argmin()]
     psr_lim = []
     dm_kde = []
     with NumpyRNGContext(1):
         boot_obs_psr = bootstrap(dm_psr, num_resamples)
+    logging.info('Bootstrapping KDE...')
     for i in range(num_resamples):
         kde_func = make_kde_funtion(grid=dm_grid, draws=boot_obs_psr[i], min_bandwidth=min_bandwidth, max_bandwidth=max_bandwidth, bandwidth_stepsize=bandwidth_stepsize, cv=cv, kernel=kernel)
         psr_p = np.diff(kde_func)
@@ -106,7 +124,10 @@ def kde_for_psr(grid, dm_psr, num_samples, num_resamples=100, min_bandwidth=3, m
     if save_to_path==None:
         pass
     else:
-        np.save(save_to_path, psr_lim)
-        np.save(save_to_path, dm_kde)
+        np.save(save_to_path+'/kde_psr_lim.npy', psr_lim)
+        np.save(save_to_path+'/kde_psr_lim_opt.npy', psr_lim_opt)
+        np.save(save_to_path+'/kde_psr.npy', dm_kde)
+        np.save(save_to_path+'/kde_psr_opt.npy', dm_kde_opt)
+        logging.info('Output saved to '+str(save_to_path))
 
     return dm_kde, psr_lim
