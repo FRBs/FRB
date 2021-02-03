26eb20ee45f843f80316c8bb05b69aba79a5ce7b
SunilSimha <sunil.simha95@gmail.com>
2021-02-02 22:57:51 -0800

    check if a coordinate is within a survey footprint

--- frb/surveys/survey_utils.py
+++ frb/surveys/survey_utils.py
@@ -8,6 +8,12 @@ from frb.surveys.psrcat import PSRCAT_Survey
 from frb.surveys import heasarc
 from frb.surveys.panstarrs import Pan_STARRS_Survey
 
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+
+import numpy as np
+import warnings
+
 allowed_surveys = ['SDSS', 'DES', 'NVSS', 'FIRST', 'WENSS', 'DECaL', 'PSRCAT', 'WISE', 'Pan-STARRS']
 
 
@@ -53,4 +59,62 @@ def load_survey_by_name(name, coord, radius, **kwargs):
     # Return
     return survey
 
+def is_inside(surveyname:str, coord:SkyCoord)->bool:
+    """
+    Tests if a coordinate is within a survey footprint.
+    Args:
+        surveyname (str): Name of the survey
+        coord (astropy.coordiantes.SkyCoord): Coordinate to check
+    Returns:
+        inside (bool): True if coord is within the footprint.
+    """
 
+    # Instantiate survey and run a cone search with 1 arcmin radius
+    survey = load_survey_by_name(surveyname, coord, 1*u.arcmin)
+    cat = survey.get_catalog()
+
+    # Are there any objects in the returned catalog?
+    if cat is None or len(cat) == 0:
+        return False
+    # is the coordinate in a region that's close to the edge but on the exterior?
+    else:
+        # Compute PAs of all returned objects
+        cat_coords = SkyCoord(cat['ra'], cat['dec'], unit="deg")
+        pa_list = coord.position_angle(cat_coords)
+
+        # If the object is in the interior, then the max PA
+        # difference is likely greater than 180 degrees. 
+        max_pa_diff = np.max(pa_list) - np.min(pa_list)
+        if np.abs(max_pa_diff) <= 180*u.deg:
+            # Are there too few objects? Warn the user if so.
+            if len(cat)<=5:
+                # Make sure there is no source at the search location.
+                # if that's not the case, then no need to warn.
+                separations = coord.separation(cat_coords)
+                if np.min(separations)>2*u.arcsec:
+                    warnings.warn("Only {} objects found in {} within 1''. Check location manually.".format(len(cat), surveyname),
+                                  RuntimeWarning, stacklevel=2)
+            return False
+        else:
+            return True
+
+def in_which_survey(coord:SkyCoord)->dict:
+    """
+    Check if a particular coord is inside any
+    survey that can be currently queried from
+    `frb.surveys` module.
+    Args:
+        coord (astropy.coordiantes.SkyCoord): Coordinate to check
+    Returns:
+        inside (dict): A dict which tells which surveys the coordinate
+            is inside.
+    """
+    # Loop through known surveys and check them one by one.
+    inside = {}
+    for surveyname in allowed_surveys:
+        # Skip PSRCAT
+        if surveyname == "PSRCAT":
+            continue
+        inside[surveyname] = is_inside(surveyname, coord)
+    
+    return inside
