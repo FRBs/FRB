4726b623229793cb3e8d7d79b76c478de1a26bd8
profxj <xavier@ucolick.org>
2021-07-02 06:41:39 -0700

    debuggin MUSE

--- frb/builds/build_specdb.py
+++ frb/builds/build_specdb.py
@@ -274,6 +274,8 @@ def generate_by_refs(input_refs, outfile, version):
         # Survey flag
         flag_g = spbu.add_to_group_dict(instr, gdict, skip_for_debug=True)
         # IDs
+        if 'MUSE' in instr:
+            embed(header='207 of build specdb')
         maindb = spbu.add_ids(maindb, full_meta, flag_g, tkeys, id_key, first=(flag_g==1), close_pairs=(instr in pair_groups))
 
         # Ingest --
