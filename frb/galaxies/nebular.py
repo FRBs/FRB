d0e76851886ae9f84a80b7a40630f0880360bc29
profxj <xavier@ucolick.org>
2021-09-14 06:49:38 -0700

    new default extinction

--- frb/galaxies/nebular.py
+++ frb/galaxies/nebular.py
@@ -208,7 +208,8 @@ def calc_SFR(neb_lines, method, z, cosmo, AV=None, curve='MW'):
 
     return SFR
 
-def get_ebv(coords,definition="SFD",region=5*units.deg,get_ext_table=False):
+def get_ebv(coords,definition="SandF",
+            region=5*units.deg,get_ext_table=False):
     """
     Get the E(B-V) value and statistic from the Milky way dust extinction
     within the query region around the input coordinate
