e1caef1b3b52a161d101aa4357927987526e49fe
SunilSimha <sunil.simha95@gmail.com>
2021-10-20 13:40:24 -0700

    correcting survey assertions for new data releases

--- frb/tests/test_frbsurveys.py
+++ frb/tests/test_frbsurveys.py
@@ -67,7 +67,7 @@ def test_des():
     des_tbl = des_srvy.get_catalog(print_query=True)
     #
     assert isinstance(des_tbl, Table)
-    assert len(des_tbl) == 1
+    assert len(des_tbl) == 2
 
 
 
@@ -80,7 +80,7 @@ def test_decals():
     decal_tbl = decal_srvy.get_catalog(print_query=True)
     #
     assert isinstance(decal_tbl, Table)
-    assert len(decal_tbl) == 2
+    assert len(decal_tbl) == 3
 
 
 
