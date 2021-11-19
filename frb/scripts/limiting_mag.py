b811854f3d6c6650b2fe7914054884c116382dd9
profxj <xavier@ucolick.org>
2021-11-18 17:09:42 -0800

    WIP

---
+++ frb/scripts/limiting_mag.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+"""
+Estimate the limiting luminosity given the magnitude limit and DM and coord
+"""
+from IPython import embed
+
+def parser(options=None):
+    import argparse
+    # Parse
+    parser = argparse.ArgumentParser(description='Script to print a summary of an FRB to the screen [v1.0]')
+    parser.add_argument("coord", type=str, help="Coordinates, e.g. J081240.7+320809 or 122.223,-23.2322 or 07:45:00.47,34:17:31.1 or FRB name (FRB180924)")
+    parser.add_argument("DM_FRB", type=float, help="FRB DM")
+    parser.add_argument("mag_limit", type=float, help="Magnitude limit in filter *without* extinction correction")
+    parser.add_argument("--filter", type=str, default='DECaL_r', help="Filter -- only used for extinction correction.  Must be a Repo approved choice")
+    parser.add_argument("--dm_hostmw", type=float, default=100., help="Assumed DM contribution from MW and Host")
+    parser.add_argument("-v", "--verbose", default=False, action="store_true", help="Overwhelm the screen?")
+
+    if options is None:
+        pargs = parser.parse_args()
+    else:
+        pargs = parser.parse_args(options)
+    return pargs
+
+
+def main(pargs):
+    """ Run
+    """
+    import json
+    import os
+    import numpy as np
+    from pkg_resources import resource_filename
+
+    from linetools import utils as ltu
+    from linetools.scripts.utils import coord_arg_to_coord
+
+    from frb.galaxies import nebular
+    from frb.galaxies import photom
+    from frb import mw
+    from frb.dm import prob_dmz
+
+    # Deal with coord
+    icoord = ltu.radec_to_coord(coord_arg_to_coord(pargs.coord))
+
+    # EBV
+    EBV = nebular.get_ebv(icoord)['meanValue']  #
+    print(f"EBV = {EBV}")
+
+    # NE 2001
+    DM_ISM = mw.ismDM(icoord)
+    print(f"NE2001 = {DM_ISM}")
+
+    # DM Cosmic
+    DM_cosmic = pargs.DM_FRB - DM_ISM.value - pargs.dm_hostmw
+
+    # Redshift estimates
+    PDM_z_grid_file = os.path.join(
+        resource_filename('frb', 'data'), 'DM',
+        'PDM_z.npz')
+    if not os.path.isfile(PDM_z_grid_file):
+        print("Generating a new PDM_z grid")
+        print("Please be patient (will take a few minutes)....")
+        #
+        z, DM, P_DM_z = prob_dmz.grid_P_DMcosmic_z()
+        # Write
+        np.savez(PDM_z_grid_file,
+                 z=z, DM=DM, PDM_z=P_DM_z)
+        print(f"File written: {PDM_z_grid_file}")
+        print("This will be used going forth")
+
+    # Load
+    print(f"Loading P(DM,z) from {PDM_z_grid_file}")
+    sdict = np.load(PDM_z_grid_file)
+    PDM_z = sdict['PDM_z']
+    z = sdict['z']
+    DM = sdict['DM']
+
+    # Do it
+    iDM = np.argmin(np.abs(DM - DM_cosmic))
+    PzDM = PDM_z[iDM, :] / np.sum(PDM_z[iDM, :])
+
+    cum_sum = np.cumsum(PzDM)
+    limits = [0.1, 0.9]
+
+    z_min = z[np.argmin(np.abs(cum_sum-limits[0]))]
+    z_max = z[np.argmin(np.abs(cum_sum-limits[1]))]
+
+    # Setup Luminosity
+
+    # Extinction correct
+    lin_A = photom.extinction_correction(pargs.filter, EBV)
+
+    embed(header='92 of limiting')
+
+# frb_mag_limit J151849.52+122235.8 200. 23.        
+    
