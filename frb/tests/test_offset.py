078c3139e688d5fa0485a11feff567d4796d45b0
profxj <xavier@ucolick.org>
2022-01-25 15:37:16 -0800

    another fix

--- frb/tests/test_offset.py
+++ frb/tests/test_offset.py
@@ -23,4 +23,4 @@ def test_offset():
     ang_avg, avg_err, ang_best, best_err = frb_offsets.angular_offset(
         ifrb, host, gal_sig=(host_ra_sig, host_dec_sig))
 
-    assert np.isclose(ang_best, 0.22686248057893754)
+    assert np.isclose(ang_best, 0.22687, rtol=1e-5)
