d26782281f2a5d7d19e3262370f5aa31e8b2fd81
SunilSimha <sunil.simha95@gmail.com>
2022-05-27 13:05:00 -1000

    anticipating DECam image catalogs

--- frb/galaxies/defs.py
+++ frb/galaxies/defs.py
@@ -116,6 +116,10 @@ NSC_bands = ['u','g', 'r', 'i', 'z', 'Y', 'VR']
 for band in NSC_bands:
     valid_filters.append('NSC_{:s}'.format(band))
 
+DECam_bands = ['u','g', 'r', 'i', 'z', 'Y', 'VR']
+for band in DECam_bands:
+    valid_filters.append("DECam_{:s}".format(band))
+
 
 # For upper limits, the flux is 3sigma and the error is set to -99.0
 valid_flux = [entry+'_flux' for entry in valid_filters]
