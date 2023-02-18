935fb73bbe0b3250c7680b1b5b97541c3a148fa0
SunilSimha <sunil.simha95@gmail.com>
2023-02-18 15:49:42 -0800

    weird merge resolve fix

--- frb/galaxies/galfit.py
+++ frb/galaxies/galfit.py
@@ -77,7 +77,6 @@ def write_cutout(cutout:Cutout2D, filename:str = "cutout.fits", overwrite:bool=F
 
 
 def _genconf(imgfile:str, psffile:str=None, mode=0,
-def _genconf(imgfile:str, psffile:str=None, mode=0,
             configfile:str=None, cdkfile:str=None, outdir:str=None, outfile:str=None,
             noisefile:str=None,
             noisefile:str=None,
