d6e516e1e32a04d4222443c7a94208ccc13be9f2
sunil.simha95@gmail.com <sunil.simha95@gmail.com>
2024-06-18 17:06:33 +0900

    bug fixes to the decals query

--- frb/surveys/decals.py
+++ frb/surveys/decals.py
@@ -75,7 +75,7 @@ class DECaL_Survey(dlsurvey.DL_Survey):
         if query is None:
             query = super(DECaL_Survey, self)._gen_cat_query(query_fields=query_fields, qtype='main')
             # include photo_z
-            query = query.replace("SELECT", "SELECT z_phot_median, z_spec, survey, z_phot_l68, z_phot_u68, z_phot_l95, z_phot_u95")
+            query = query.replace("SELECT", "SELECT z_phot_median, z_spec, survey, z_phot_l68, z_phot_u68, z_phot_l95, z_phot_u95,")
             query = query.replace("ls_id", "t.ls_id")
             query = query.replace("brickid", "t.brickid")
             query = query.replace(f"FROM {self.database}\n", f"FROM {self.database} as t LEFT JOIN {self.database.split('.')[0]}.photo_z AS p ON t.ls_id=p.ls_id\n")
