d1445218c68a6b7e57023692df9a7035a42ba1c4
profxj <xavier@ucolick.org>
2024-11-09 17:15:49 -0800

    floats

--- frb/galaxies/nebular.py
+++ frb/galaxies/nebular.py
@@ -70,8 +70,8 @@ def calc_dust_extinct(neb_lines, method):
     #a1AV = extinction.fm07(np.atleast_1d(wave1), 1.0)[0]
     #a2AV = extinction.fm07(np.atleast_1d(wave2), 1.0)[0]
     extmod = dust_extinction.parameter_averages.G23(Rv=3.1)
-    a1AV = extmod(np.atleast_1d(wave1*units.AA))  # *units.AA)
-    a2AV = extmod(np.atleast_1d(wave2*units.AA))  # *units.AA)
+    a1AV = extmod(np.atleast_1d(wave1*units.AA))[0]  # *units.AA)
+    a2AV = extmod(np.atleast_1d(wave2*units.AA))[0]  # *units.AA)
 
     # Observed ratio
     fratio_obs = F1_obs/F2_obs
