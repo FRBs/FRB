cede07db9c46012e0257eb3fac2bbcd4c748fd4d
Mawson Sammons <mawsonsammons@mawsons-mbp.dhcp.asu.edu>
2025-12-09 15:11:55 -0700

    bug in tables use

--- frb/surveys/cluster_search.py
+++ frb/surveys/cluster_search.py
@@ -94,7 +94,7 @@ class TullyGroupCat(VizierCatalogSearch):
         
         # Convert distances from h^-1 Mpc to Mpc based on the cosmology being used.
         catalog['Dist'] /=self.cosmo.h
-        rec_velocity = catalog['Dist']*self.cosmo.H0.value
+        rec_velocity = catalog['Dist'].value*self.cosmo.H0.value
         c_kms = 299792.458
         redshift = ((1+rec_velocity/c_kms)/(1-rec_velocity/c_kms))**0.5-1
         catalog['Dist'] = self.cosmo.angular_diameter_distance(redshift).value
