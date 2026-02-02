added lines to query more recent release of delve if desired, and fixed a astropy units Mpc issue with nedlvs that I encountered

frb/surveys/nedlvs.py
@@ -36,7 +36,8 @@ class NEDLVS(surveycoord.SurveyCoord):
 
         # Set redshift distances using the cosmology of choice
         redshift_dist_sources = self.datatab['DistMpc_method']=='Redshift'
-        self.datatab['DistMpc'][redshift_dist_sources] = self.cosmo.luminosity_distance(self.datatab['z'][redshift_dist_sources]).to('Mpc').value
+        # self.datatab['DistMpc'][redshift_dist_sources] = self.cosmo.luminosity_distance(self.datatab['z'][redshift_dist_sources]).to('Mpc').value
+        self.datatab['DistMpc'][redshift_dist_sources] = self.cosmo.luminosity_distance(self.datatab['z'][redshift_dist_sources])*u.Mpc
         self.datatab['phys_sep'] = self.datatab['DistMpc']*u.Mpc*np.sin(self.datatab['ang_sep'].to('rad').value)
     
     def get_column_names(self):